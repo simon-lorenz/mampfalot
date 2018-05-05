3eb216707464e50e9e1b84bf16b915c8c6b374db
Simon Lorenz <simon@lorenz-nrw.de>
2018-05-05 20:06:51 +0200

    Adds route POST /foodTypes

--- router/foodTypes.js
+++ router/foodTypes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const FoodType = require('./../models/foodType')
+const util = require('./../util/util')
 
 router.route('/').get((req, res) => {
     FoodType.findAll({
@@ -15,5 +16,26 @@ router.route('/').get((req, res) => {
         res.status(400).send('Ein Fehler ist aufgetreten' + error)
     })
 });
+
+router.route('/').post(util.isAdmin, (req, res) => {
+    let foodType = {
+        type: req.body.type
+    }
+
+    let missingValues = util.missingValues(foodType)
+    if (missingValues.length > 0) {
+        res.status(400).send({ missingValues })
+        return
+    }
+
+    FoodType.create(foodType)
+    .then(result => {
+        res.send(result)
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).send('Something went wrong.')
+    })
+})
 
 module.exports = router
