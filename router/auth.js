badbbadcbaccc581b11962e7cfaaa0afb06a3278
Simon Lorenz <simon@lorenz-nrw.de>
2018-06-19 20:07:02 +0200

    Refactor

--- router/auth.js
+++ router/auth.js
@@ -6,32 +6,29 @@ const User = require('./../models').User
 const middleware = require('./../middleware/auth')
 
 router.route('/').get(middleware.basicAuth, async (req, res) => {
-	let credentials = res.locals.credentials
-
 	try {
 		let user = await User.unscoped().findOne({ 
 			where: { 
-				email: credentials.email 
+				email: res.locals.credentials.email 
 			},
 			attributes: ['id', 'name', 'email', 'password'],
 			raw: true 
 		})
 
 		// Prüfe, ob der User vorhanden ist und ob sein Passwort übereinstimmt
-		if (!user || !bcrypt.compareSync(credentials.password, user.password)) {
+		if (!user || !bcrypt.compareSync(res.locals.credentials.password, user.password)) {
 			res.status(401).send('Invalid credentials.')
 			return
 		}
 
-		// Passwort entfernen, damit es nicht im token landet
+		// Passwort entfernen, damit es nicht im Token landet
 		user.password = undefined
 
 		// Generiere Token
-		let token = jwt.sign(user, process.env.SECRET_KEY, {
-			expiresIn: '10h'
-		})
-
+		let token = jwt.sign(user, process.env.SECRET_KEY, { expiresIn: '10h' })
+		
 		res.send({ token })
+
 	} catch (error) {
 		console.log(error)
 		res.status(500).send()
