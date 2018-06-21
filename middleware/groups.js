888221bc2e8945cc67eec8ebbe863c0318b2eaad
Simon Lorenz <simon@lorenz-nrw.de>
2018-06-21 15:38:59 +0200

    Refactor

---
+++ middleware/groups.js
@@ -0,0 +1,82 @@
+const Group = require('./../models').Group
+const FoodType = require('./../models').FoodType
+const Place = require('./../models').Place
+const User = require('./../models').User
+const Lunchbreak = require('./../models').Lunchbreak
+const Util = require('./../util/util')
+
+module.exports = {
+  findAllGroups: function (req, res, next) {
+    Group.findAll({
+			where: {
+				id: { in: Util.getGroupIds(res.locals.user, false)
+				}
+			},
+			include: [
+				Place,
+				FoodType,
+				Lunchbreak,
+				{
+					model: User,
+					as: 'members',
+					through: {
+						as: 'config',
+						attributes: ['color', 'authorizationLevel']
+					}
+				}
+			]
+		})
+		.then(groups => {
+			res.send(groups)
+		})
+  },
+  loadGroup: function (req, res, next) {
+    Group.findOne({
+      where: {
+        id: req.params.groupId
+      },
+      include: [
+        {
+          model: Place,
+          attributes: {
+            exclude: ['foodTypeId', 'groupId']
+          },
+          include: [ FoodType ]
+        },
+        {
+          model: FoodType,
+          attributes: {
+            exclude: ['groupId']
+          }
+        },
+        {
+          model: Lunchbreak,
+          limit: parseInt(req.query.lunchbreakLimit) || 25
+        },
+        {
+          model: User,
+          as: 'members',
+          through: {
+            as: 'config',
+            attributes: ['color', 'authorizationLevel']
+          }
+        }
+      ]
+    })
+    .then(group => {
+      res.locals.group = group
+      next()
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).send()
+    })
+  },
+  groupExists: function (req, res, next) {
+    if (res.locals.group) {
+      next()
+    } else {
+      res.status(404).send()
+    }
+  }
+}
--- router/groups.js
+++ router/groups.js
@@ -1,90 +1,20 @@
-const Group = require('./../models').Group
 const router = require('express').Router()
-const GroupMembers = require('./../models').GroupMembers
 const FoodType = require('./../models').FoodType
 const Place = require('./../models').Place
-const User = require('./../models').User
-const Lunchbreak = require('./../models').Lunchbreak
-const Util = require('./../util/util')
-const sec = require('./../util/sec')
+const middleware = require('./../middleware/groups')
 const commonMiddleware = require('./../middleware/common')
 
-router.route('/').get((req, res) => {
-	Group.findAll({
-			where: {
-				id: { in: Util.getGroupIds(res.locals.user, false)
-				}
-			},
-			include: [
-				Place,
-				FoodType,
-				Lunchbreak,
-				{
-					model: User,
-					as: 'members',
-					through: {
-						as: 'config',
-						attributes: ['color', 'authorizationLevel']
-					}
-				}
-			]
-		})
-		.then(groups => {
-			res.send(groups)
-		})
-})
+router.route('/').get(middleware.findAllGroups)
 
-router.param('groupId', (req, res, next) => {
-	// 1. Lade die angefragte Gruppe
-	Group.findOne({
-		where: {
-			id: req.params.groupId
-		},
-		include: [
-			{
-				model: Place,
-				attributes: {
-					exclude: ['foodTypeId', 'groupId']
-				},
-				include: [ FoodType ]
-			},
-			{
-				model: FoodType,
-				attributes: {
-					exclude: ['groupId']
-				}
-			},
-			{
-				model: Lunchbreak,
-				limit: parseInt(req.query.lunchbreakLimit) || 25
-			},
-			{
-				model: User,
-				as: 'members',
-				through: {
-					as: 'config',
-					attributes: ['color', 'authorizationLevel']
-				}
-			}
-		]
-	})
-	.then(group => {
-		res.locals.group = group
-		next()
-	})
-	.catch(err => {
-		console.log(err)
-		res.status(500).send()
-	})
-})
+// 1. Lade die angefragte Gruppe
+router.param('groupId', middleware.loadGroup)
 
 // 2. Existiert die Gruppe (war 1. erfolgreich)?
-router.param('groupId', groupExists)
+router.param('groupId', middleware.groupExists)
 
 // 3. Ist der User Mitglied der Gruppe?´
 router.param('groupId', (req, res, next) => {
 	commonMiddleware.userIsGroupMember(res.locals.group.id)(req, res, next)
-	// x(req, res, next)
 })
 
 router.route('/:groupId').get((req, res) => {
@@ -92,49 +22,15 @@ router.route('/:groupId').get((req, res) => {
 })
 
 router.route('/:groupId/members').get((req, res) => {
-	Group.findOne({
-		where: {
-			id: req.params.groupId
-		},
-		include: [
-			{
-				model: User,
-				as: 'members',
-				through: {
-					as: 'config',
-					attributes: ['color', 'authorizationLevel']
-				}
-			}
-		]
-	})
-	.then(group => {
-		res.send(group.members)
-	})
+	res.send(res.locals.group.members)
 })
 
 router.route('/:groupId/lunchbreaks').get((req, res) => {
-	Lunchbreak.findAll({
-			where: {
-				groupId: req.params.groupId
-			}
-		})
-		.then(lunchbreaks => {
-			res.send(lunchbreaks)
-		})
-		.catch(err => {
-			res.send(err)
-		})
+	res.send(res.locals.group.lunchbreaks)
 })
 
 router.route('/:groupId/foodTypes').get((req, res) => {
-	FoodType.findAll({
-		where: {
-			groupId: req.params.groupId
-		}
-	})
-	.then(foodTypes => {
-		res.send(foodTypes)
-	})
+	res.send(res.locals.group.foodTypes)
 })
 
 router.route('/:groupId/foodTypes').post(commonMiddleware.userIsGroupAdmin, (req, res) => {
@@ -151,14 +47,7 @@ router.route('/:groupId/foodTypes').post(commonMiddleware.userIsGroupAdmin, (req
 })
 
 router.route('/:groupId/places').get((req, res) => {
-	Place.findAll({
-		where: {
-			groupId: req.params.groupId
-		}
-	})
-	.then(places => {
-		res.send(places)
-	})
+	res.send(res.locals.group.places)
 })
 
 router.route('/:groupId/places').post(commonMiddleware.userIsGroupAdmin, (req, res) => {
@@ -174,21 +63,5 @@ router.route('/:groupId/places').post(commonMiddleware.userIsGroupAdmin, (req, r
 			res.status(400).send(err)
 		})
 })
-
-async function groupExists(req, res, next) {
-	await Group.findOne({
-			where: {
-				id: req.params.groupId
-			},
-			raw: true
-		})
-		.then(group => {
-			if (group) {
-				next()
-			} else {
-				res.status(404).send()
-			}
-		})
-}
 
 module.exports = router
