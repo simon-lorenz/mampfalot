f043f3d42cb5b83e732a47fd24f9425529b41051
Simon Lorenz <simon@lorenz-nrw.de>
2018-07-03 08:00:57 +0200

    Refactor

--- router/foodTypes.js
+++ router/foodTypes.js
@@ -4,21 +4,6 @@ const FoodType = require('./../models').FoodType
 const middleware = require('../middleware/foodTypes')
 const commonMiddleware = require('../middleware/common')
 
-
-router.route('/').get((req, res) => {
-	FoodType.findAll({
-			order: [
-				['id', 'ASC']
-			]
-		})
-		.then(result => {
-			res.send(result)
-		})
-		.catch(error => {
-			res.status(400).send('Ein Fehler ist aufgetreten' + error)
-		})
-})
-
 router.route('/').post(middleware.postFoodType)
 
 router.use('/:foodTypeId*', middleware.loadFoodType)
@@ -54,23 +39,14 @@ router.route('/:foodTypeId').put((req, res) => {
 		})
 })
 
-router.route('/:foodTypeId').delete((req, res) => {
-	FoodType.destroy({
-			where: {
-				id: req.params.foodTypeId
-			}
-		})
-		.then(result => {
-			if (result === 0) {
-				res.status(404).send()
-			} else {
-				res.status(204).send()
-			}
-		})
-		.catch(error => {
-			console.log(error)
-			res.status(500).send('Something went wrong.')
-		})
+router.route('/:foodTypeId').delete(async (req, res) => {
+	try {
+		await res.locals.foodType.destroy()
+		res.status(204).send()
+	} catch (error) {
+		console.log(error)
+		res.status(500).send()
+	}
 })
 
 module.exports = router
