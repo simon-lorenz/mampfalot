28d952d61d0c3c357523bc96cac92d32140f80fe
Simon Lorenz <simon@lorenz-nrw.de>
2018-07-04 17:14:49 +0200

    more tests for route lunchbreaks

--- test/tests/lunchbreaks.js
+++ test/tests/lunchbreaks.js
@@ -61,9 +61,47 @@ module.exports = (request, bearerToken) => {
                 firstParticipant.should.have.property('lunchbreakId').equal(1)
                 firstParticipant.should.have.property('userId').equal(1)
                 firstParticipant.should.have.property('lunchTimeSuggestion')
-                // firstParticipant.should.not.have.property('amountSpent')
+                firstParticipant.should.not.have.property('amountSpent')
               })
               .end(done)
+          })
+        })
+
+        describe('/:participantId', () => {
+          it('sends a valid participant resource', (done) => {
+            request
+              .get('/lunchbreaks/1/participants/1')
+              .set({ Authorization: bearerToken[1] })
+              .expect(200)
+              .expect(res => {
+                let participant = res.body
+                participant.should.have.property('id').equal(1)
+                participant.should.have.property('lunchbreakId').equal(1)
+                participant.should.have.property('userId').equal(1)
+                participant.should.have.property('lunchTimeSuggestion')
+                participant.should.not.have.property('amountSpent')
+              })
+              .end(done)
+            })
+
+          describe('/votes', () => {
+            it('sends a list of votes', (done) => {
+              request 
+              .get('/lunchbreaks/1/participants/1/votes')
+              .set({ Authorization: bearerToken[1] })
+              .expect(200)
+              .expect(res => {
+                let votes = res.body
+                votes.should.be.an('array').which.has.a.lengthOf(3)
+                
+                let firstVote = votes[0]
+                firstVote.should.have.property('id').equal(1)
+                firstVote.should.have.property('participantId').equal(1)
+                firstVote.should.have.property('placeId').equal(2)
+                firstVote.should.have.property('points').equal(30)
+              })
+              .end(done)
+            })
           })
         })
       }) 
