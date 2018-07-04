d4f35b487a9874965a9e7ff3d7767f07ccaad768
Simon Lorenz <simon@lorenz-nrw.de>
2018-07-04 17:15:41 +0200

    Rewrites router lunchbreaks

--- router/lunchbreaks.js
+++ router/lunchbreaks.js
@@ -6,147 +6,33 @@ const Participant = require('./../models').Participant
 const Vote = require('./../models').Vote
 const Place = require('./../models').Place
 const User = require('./../models').User
-const Util = require('./../util/util')
-const Sec = require('./../util/sec')
-const LunchbreakMiddleware = require('./../middleware/lunchbreaks')
+const middleware = require('./../middleware/lunchbreaks')
 
-// Liefert alle Lunchbreaks der Gruppen des Users
-router.route('/').get((req, res, next) => {
-	Lunchbreak.findAll({
-		where: {
-			groupId: {
-				in: Util.getGroupIds(res.locals.user, false)
-			}
-		}
-	})
-	.then(lunchbreaks => {
-		res.send(lunchbreaks)
-	})
-	.catch(err => {
-		next(err)
-	})
-})
-
-router.use('/:lunchbreakId*', [Sec.userHasAccessToLunchbreak, LunchbreakMiddleware.loadGroupInfoByLunchbreakId])
+router.param('lunchbreakId', middleware.loadLunchbreak)
 
 router.route('/:lunchbreakId').get((req, res, next) => {
-	Lunchbreak.findOne({
-		where: {
-			groupId: {
-				and: {
-					eq: req.params.lunchbreakId,
-					in: Util.getGroupIds(res.locals.user, false)
-				}
-			}
-		}
-	})
-	.then(lunchbreak => {
-		if (lunchbreak) {
-			res.send(lunchbreak)
-		} else {
-			res.status(400).send()
-		}
-	})
-	.catch(err => {
-		next(err)
-	})
+	res.send(res.locals.lunchbreak)
+})
+
+router.route('/:lunchbreakId/comments').get((req, res, next) => {
+	res.send(res.locals.lunchbreak.comments)
 })
 
 router.route('/:lunchbreakId/participants').get((req, res, next) => {
-	Participant.findAll({
-		where: {
-			lunchbreakId: req.params.lunchbreakId
-		},
-		attributes: {
-			exclude: ['amountSpent', 'userId']
-		},
-		include: [ 
-			{
-				model: User
-			},
-			{
-				model: Vote,
-				attributes: {
-					exclude: ['id', 'participantId', 'placeId']
-				},
-				include: [ 
-					{
-						model: Place,
-						attributes: {
-							exclude: ['id', 'groupId']
-						}
-					} 
-				]
-			} 
-		]
-	})
-	.then(participants => {
-		res.send(participants)
-	})
-	.catch(err => {
-		next(err)
-	})
+	res.send(res.locals.lunchbreak.participants)
 })
 
+router.param('participantId', middleware.loadParticipant)
+
 router.route('/:lunchbreakId/participants/:participantId').get((req, res, next) => {
-	Participant.findOne({
-		where: {
-			id: req.params.participantId
-		},
-		include: [ 
-			{
-				model: User
-			},
-			{
-				model: Vote,
-				attributes: {
-					exclude: ['id', 'participantId', 'placeId']
-				},
-				include: [ 
-					{
-						model: Place,
-						attributes: {
-							exclude: ['id', 'groupId']
-						}
-					} 
-				]
-			}
-		]
-	})
-	.then(participant => {
-		res.send(participant)
-	})
-	.catch(err => {
-		next(err)
-	})
+	res.send(res.locals.participant)
 })
 
 router.route('/:lunchbreakId/participants/:participantId/votes').get((req, res, next) => {
-	Vote.findAll({
-		where: {
-			participantId: req.params.participantId
-		},
-		attributes: {
-			exclude: ['id', 'participantId', 'placeId']
-		},
-		include: [ 
-			{
-				model: Place,
-				attributes: {
-					exclude: ['id', 'groupId']
-				}
-			} 
-		]
-	})
-	.then(votes => {
-		res.send(votes)
-	})
-	.catch(err => {
-		next(err)
-	})
+	res.send(res.locals.participant.votes)
 })
 
-router.route('/:lunchbreakId/participants/:participantId/votes').post(LunchbreakMiddleware.checkVotes, async function (req, res, next) {
+router.route('/:lunchbreakId/participants/:participantId/votes').post(middleware.checkVotes, async function (req, res, next) {
 	let votes = req.body.votes
 	
 	try {
@@ -172,22 +58,5 @@ router.route('/:lunchbreakId/participants/:participantId/votes').post(Lunchbreak
 	}
 })
 
-router.route('/:lunchbreakId/comments').get((req, res, next) => {
-	Comment.findAll({
-		where: {
-			lunchbreakId: req.params.lunchbreakId
-		},
-		attributes: {
-			exclude: ['lunchbreakId', 'groupId', 'userId']
-		},
-		include: [ User ]
-	})
-	.then(comments => {
-		res.send(comments)
-	})
-	.catch(err => {
-		next(err)
-	})
-})
 
 module.exports = router
