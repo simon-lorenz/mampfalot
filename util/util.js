7a870a7ebf38ba2eb4b3660c8c295327a7b9f76c
Simon Lorenz <simon@lorenz-nrw.de>
2018-07-06 21:57:29 +0200

    Removes dead util code

--- util/util.js
+++ util/util.js
@@ -1,32 +1,5 @@
 let Util = {}
 
-Util.addKeyIfExists = function (from, to, key) {
-	if (key in from) {
-		to[key] = from[key]
-	}
-}
-
-Util.missingValues = function (obj) {
-	let undefinedKeys = []
-	for (key in obj) {
-		if (!obj[key]) {
-			undefinedKeys.push(key)
-		}
-	}
-	return undefinedKeys
-}
-
-Util.findDuplicates = function (arr) {
-	let duplicates = []
-	let sorted = arr.slice().sort()
-	for (let i = 0; i < sorted.length; i++) {
-		if (sorted[i] == sorted[i + 1]) {
-			duplicates.push(sorted[i])
-		}
-	}
-	return duplicates
-}
-
 Util.getGroupIds = function (user, adminOnly = false) {
 	let groupIds = []
 	for (group of user.groups) {
