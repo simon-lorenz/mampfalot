4938c0ea18686c7d79e5df9b9b061e3a64df8241
Simon Lorenz <simon@lorenz-nrw.de>
2018-07-06 16:00:00 +0200

    test suite refactoring

--- test/tests/lunchbreaks.js
+++ test/tests/lunchbreaks.js
@@ -130,39 +130,43 @@ module.exports = (request, bearerToken) => {
         })
 
         describe('/:participantId', () => {
-          it('sends a valid participant resource', (done) => {
-            request
-              .get('/lunchbreaks/1/participants/1')
-              .set({ Authorization: bearerToken[1] })
-              .expect(200)
-              .expect(res => {
-                let participant = res.body
-                participant.should.have.property('id').equal(1)
-                participant.should.have.property('lunchbreakId').equal(1)
-                participant.should.have.property('userId').equal(1)
-                participant.should.have.property('lunchTimeSuggestion')
-                participant.should.not.have.property('amountSpent')
-              })
-              .end(done)
+          describe('GET', () => {
+            it('sends a valid participant resource', (done) => {
+              request
+                .get('/lunchbreaks/1/participants/1')
+                .set({ Authorization: bearerToken[1] })
+                .expect(200)
+                .expect(res => {
+                  let participant = res.body
+                  participant.should.have.property('id').equal(1)
+                  participant.should.have.property('lunchbreakId').equal(1)
+                  participant.should.have.property('userId').equal(1)
+                  participant.should.have.property('lunchTimeSuggestion')
+                  participant.should.not.have.property('amountSpent')
+                })
+                .end(done)
             })
+          })
 
           describe('/votes', () => {
-            it('sends a list of votes', (done) => {
-              request 
-              .get('/lunchbreaks/1/participants/1/votes')
-              .set({ Authorization: bearerToken[1] })
-              .expect(200)
-              .expect(res => {
-                let votes = res.body
-                votes.should.be.an('array').which.has.a.lengthOf(3)
-                
-                let firstVote = votes[0]
-                firstVote.should.have.property('id').equal(1)
-                firstVote.should.have.property('participantId').equal(1)
-                firstVote.should.have.property('placeId').equal(2)
-                firstVote.should.have.property('points').equal(30)
+            describe('GET', () => {
+              it('sends a list of votes', (done) => {
+                request 
+                .get('/lunchbreaks/1/participants/1/votes')
+                .set({ Authorization: bearerToken[1] })
+                .expect(200)
+                .expect(res => {
+                  let votes = res.body
+                  votes.should.be.an('array').which.has.a.lengthOf(3)
+                  
+                  let firstVote = votes[0]
+                  firstVote.should.have.property('id').equal(1)
+                  firstVote.should.have.property('participantId').equal(1)
+                  firstVote.should.have.property('placeId').equal(2)
+                  firstVote.should.have.property('points').equal(30)
+                })
+                .end(done)
               })
-              .end(done)
             })
           })
         })
