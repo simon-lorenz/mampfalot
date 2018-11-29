2bc0acadf5883372bffd64651603e710ef1c3256
Simon Lorenz <dev@simon-lorenz.net>
2018-11-29 21:16:24 +0100

    Improves logging at app launch

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const app = require('./app')
 const PORT = process.env.PORT || 5000
+const sequelize = require('./models').sequelize
 
 app.listen(PORT, () => {
-	console.log('Running in ' + process.env.NODE_ENV + ' mode.')
-	console.log('Database: ' + process.env.DB_NAME)
+	console.log(`Launched in ${process.env.NODE_ENV} mode.`)
 	console.log('Listening to port ' + PORT)
+	console.log(`Trying to connect to ${process.env.DB_NAME} at ${process.env.DB_HOST} ...`)
+	sequelize.authenticate()
+		.then(() => console.log('Database connection successfully established.'))
+		.catch((err) => {
+			console.error('Database connection could not be established.')
+			console.error(err.toString())
+		})
 })
