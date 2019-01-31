989a21445c43bb4040b3f1c6f179004217b5101c
silo0815 <dev@simon-lorenz.net>
2019-01-31 12:03:30 +0000

    Removes references to undefined env vars

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.listen(PORT, () => {
 	console.log(`[Config] Launched in ${process.env.NODE_ENV} mode.`)
 	console.log(`[Config] Listening to port ${PORT}`)
 	console.log(`[Config] Frontend URL: ${process.env.FRONTEND_BASE_URL}`)
-	console.log(`[Database] Trying to connect to "${process.env.DB_NAME}" at ${process.env.DB_HOST} ...`)
+	console.log('[Database] Trying to connect to the database ...')
 	sequelize.authenticate()
 		.then(() => console.log('[Database] Connection successfully established.'))
 		.catch((err) => {
