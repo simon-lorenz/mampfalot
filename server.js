5adea9d54369cfcbeb8ac9691bc5fe5050174676
Simon <dev@simon-lorenz.net>
2019-02-01 16:31:09 +0100

    Sync database on server launch

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ app.listen(PORT, () => {
 	console.log('[Database] Trying to connect to the database ...')
 	sequelize.authenticate()
 		.then(() => console.log('[Database] Connection successfully established.'))
+		.then(async () => {
+			try {
+				console.log('[Database] Initializing sync...')
+				await sequelize.sync()
+				console.log('[Database] Sync complete!')
+			} catch (err) {
+				console.log('[Database] Sync failed!')
+				console.error(JSON.stringify(err))
+				console.info('Shutting down...')
+				process.exit()
+			}
+		})
 		.catch((err) => {
 			console.error('[Database] Connection could not be established.')
 			console.error(JSON.stringify(err))
