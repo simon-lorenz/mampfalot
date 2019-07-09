3da231e1c7347c1b18884ff77ec76c687ba16c73
Simon Lorenz <mail@simon-lorenz.dev>
2019-07-09 16:37:07 +0200

    better readability and removal of dead code

--- router/groups.js
+++ router/groups.js
@@ -1,23 +1,23 @@
 'use strict'
 
 const router = require('express').Router()
-const { Place } = require('../models')
 const { allowMethods, hasBodyValues, convertParamToNumber } = require('../util/middleware')
 const { asyncMiddleware } = require('../util/util')
-const user = require('../classes/user')
 const GroupController = require('../controllers/group-controller')
 const PlaceRouter = require('./places')
 const LunchbreakRouter = require('./lunchbreaks')
 const GroupMemberRouter = require('./group-members')
 const InvitationRouter = require('./invitiations')
 
+router.use('/:groupId/members', GroupMemberRouter)
+router.use('/:groupId/invitations', InvitationRouter)
+router.use('/:groupId/places', PlaceRouter)
+router.use('/:groupId/lunchbreaks', LunchbreakRouter)
+
 router.route('/').all(allowMethods(['POST']))
 router.route('/').post(hasBodyValues(['name'], 'all'))
 router.route('/:groupId').all(allowMethods(['GET', 'PUT', 'DELETE']))
 router.route('/:groupId').put(hasBodyValues(['name', 'lunchTime', 'voteEndingTime', 'utcOffset', 'pointsPerDay', 'maxPointsPerVote', 'minPointsPerVote'], 'atLeastOne'))
-
-router.use('/:groupId/members', GroupMemberRouter)
-router.use('/:groupId/invitations', InvitationRouter)
 
 router.route('/').post(asyncMiddleware(async (req, res, next) => {
 	const values = req.body
@@ -41,25 +41,6 @@ router.route('/:groupId').delete(asyncMiddleware(async (req, res, next) => {
 	const { groupId } = req.params
 	await GroupController.deleteGroup(groupId)
 	res.status(204).send()
-}))
-
-router.use('/:groupId/places', PlaceRouter)
-
-router.use('/:groupId/lunchbreaks', LunchbreakRouter)
-
-router.route('/:groupId/places').get((req, res) => {
-	res.send(res.locals.group.places)
-})
-
-router.route('/:groupId/places').post(asyncMiddleware(async (req, res, next) => {
-	const place = Place.build({
-		groupId: parseInt(req.params.groupId),
-		foodType: req.body.foodType,
-		name: req.body.name
-	})
-
-	await user.can.createPlace(place)
-	res.status(201).send(await place.save())
 }))
 
 module.exports = router
