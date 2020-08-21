9d1544811cce5e68332cbc49bbe62f844394b866
Simon Lorenz <mail@simon-lorenz.dev>
2020-08-21 23:07:59 +0200

    test: wait up to 5 seconds for the database

--- test/mocha-globals.js
+++ test/mocha-globals.js
@@ -27,6 +27,26 @@ const testServer = require('./utils/test-server')
 const knex = require('../src/knex')
 
 before(async () => {
+	// Check database connection
+	const max_tries = 5
+	let tries = 1
+
+	while (tries <= max_tries) {
+		try {
+			await knex.raw('SELECT 1+1 AS result')
+			break
+		} catch (error) {
+			if (tries === max_tries) {
+				console.error(`[Database] Connection could not be established: ${error}`)
+				process.exit(1)
+			} else {
+				console.log(`[Database] Could not connect to database (${tries}/${max_tries}).`)
+				tries++
+				await new Promise(resolve => setTimeout(resolve, 1000))
+			}
+		}
+	}
+
 	await knex.migrate.rollback(true)
 	await knex.migrate.latest()
 })
